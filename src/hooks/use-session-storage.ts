0724562eaded49ecccf3b51af829112cd0e00dfc
meunomeebero <me@bero.land>
2024-04-12 12:46:26 -0300

    fix: solve no window defined error

--- src/hooks/use-session-storage.ts
+++ src/hooks/use-session-storage.ts
@@ -1,5 +1,7 @@
 import { useMemo } from "react";
 
+let window: Window | undefined;
+
 export function useSessionStorage() {
     const sessionStorage = useMemo(() => {
         if (typeof window !== 'undefined') {
