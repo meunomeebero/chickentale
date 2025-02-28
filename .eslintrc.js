2d7eed738a1a382d1ccba7667d78cfa1906eddfd
meunomeebero <me@bero.land>
2025-02-28 14:42:03 -0300

    chore: configure ESLint with Next.js core web vitals and custom rules

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: 'next/core-web-vitals',
+  rules: {
+    '@next/next/no-img-element': 'off',
+    'react/no-unescaped-entities': 'off',
+    'jsx-a11y/alt-text': 'off'
+  },
+};
